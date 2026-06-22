142c9acbb216c5d573dd8bf2c83dcdae96a0c5d0
s44-star <spraharaj68@gmail.com>
2026-06-22 15:21:10 +0530

    Delete sw.js

--- sw.js
+++
@@ -1,3 +0,0 @@
-const CACHE='lx-v1';
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['/']))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
